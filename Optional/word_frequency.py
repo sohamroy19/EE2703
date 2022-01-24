72c2015e4cb80eb2e772b3a3543417f6f07111a3
royS <72430659+sohamroy19@users.noreply.github.com>
2022-01-24 18:09:46 +0530

    Added optional task

---
+++ Optional/word_frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Counts frequency of each word in the file `text.txt` """
+
+import re   # regular expressions
+
+dict = {}
+with open("text.txt", encoding="utf8") as file:
+    for line in file:
+        words = re.split("[^a-z]+", line.lower())
+        for word in words:
+            if word in dict:
+                dict[word] += 1
+            elif word != "":
+                dict[word] = 1
+
+print(sorted(dict.items(), key=lambda x: x[1], reverse=True))
